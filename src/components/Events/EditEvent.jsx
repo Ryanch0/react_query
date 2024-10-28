6d148badb2f730bcd348e3d8b12722848ea4e4c4
Sungwoo <tjddnfkdls0@naver.com>
2024-10-28 15:42:15 +0900

    react router with react-query

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Link, redirect, useNavigate, useParams } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
@@ -13,7 +13,6 @@ export default function EditEvent() {
   // 1. id로 데이터를 먼저 가져와서 폼을 채운다
   const {
     data: detailData,
-    isPending: isQueryPending,
     isError: isQueryError,
     error: queryError
   } = useQuery({
@@ -29,22 +28,22 @@ export default function EditEvent() {
     // error: mutationError
   } = useMutation({
     mutationFn: updateEvent,
-    onMutate: async(data) => { // mutate를 호출하는 즉시 실행
-      await queryClient.cancelQueries({queryKey: ['events', {eventId: id}]}) // 특정 키의 모든 활성 쿼리 취소 (낙관적 업데이트의 데이터와 충돌하지 않기위해)
-      const previousEvent = queryClient.getQueryData(['events', {eventId: id}]) //롤백용 이전 데이터
-      queryClient.setQueryData(['events', {eventId: id}], data.event )
+    onMutate: async (data) => { // mutate를 호출하는 즉시 실행
+      await queryClient.cancelQueries({ queryKey: ['events', { eventId: id }] }) // 특정 키의 모든 활성 쿼리 취소 (낙관적 업데이트의 데이터와 충돌하지 않기위해)
+      const previousEvent = queryClient.getQueryData(['events', { eventId: id }]) //롤백용 이전 데이터
+      queryClient.setQueryData(['events', { eventId: id }], data.event)
       // 첫번째인자 : 가져오려는 쿼리 키, 두번째 인자: 변경하려는 새 데이터
       // mutate로 전달한 데이터는 즉시 onMutate에 전달됨
-      
-      return {previousEvent} // onError에서 context에 해당함
+
+      return { previousEvent } // onError에서 context에 해당함
     },
     onError: (error, data, context) => { // mutate가 실패할경우 실행되는 함수
-      queryClient.setQueryData(['events', {eventId: id}], context.previousEvent ) // 롤백
+      queryClient.setQueryData(['events', { eventId: id }], context.previousEvent) // 롤백
 
     },
     onSettled: () => { // mutation이 완료될때마다 호출(실패,성공 상관없이)
-      queryClient.invalidateQueries(['events', {eventId: id}]) //성공하든, 실패해서 롤백되던 최신데이터를 가져옴
-    } 
+      queryClient.invalidateQueries(['events', { eventId: id }]) //성공하든, 실패해서 롤백되던 최신데이터를 가져옴
+    }
   })
 
 
@@ -63,12 +62,11 @@ export default function EditEvent() {
 
   return (
     <Modal onClose={handleClose}>
-      {isQueryPending && <p>Loading ...</p>}
       {isQueryError && <ErrorBlock
         title='Failed to fetch event'
         message={queryError.info?.message || 'Failed to fetch event'}
       />}
-      {!isQueryPending && detailData && (
+      {detailData && (
         <EventForm inputData={detailData || {}} onSubmit={handleSubmit}>
           <Link to="../" className="button-text">
             Cancel
@@ -85,4 +83,20 @@ export default function EditEvent() {
       />} */}
     </Modal>
   );
+}
+
+export function loader({ params }) {
+  const { id } = params
+  return queryClient.fetchQuery({
+    queryKey: ['events', { eventId: id }],
+    queryFn: ({ signal }) => fetchEvent({ id, signal })
+  })
+}
+
+export async function action({request, params}) {
+  const formData = await request.formData()
+  const updatedEventData = Object.fromEntries(formData)
+  await updateEvent({id: params.id, event : updatedEventData})
+  await queryClient.invalidateQueries(['events'])
+  return redirect('../')
 }
